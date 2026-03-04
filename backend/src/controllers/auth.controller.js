26ddac4d3403aa4eb7aeddfe89d3bcd9d66a4a2b
hassan00-0 <hassanyassine0987@gmail.com>
2026-03-03 23:30:27 -0300

    make hte signup route work

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,49 @@
+import { generateToken } from "../lib/utils";
+import User from "../models/user.model";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../lib/utils";
+
+export const signUp = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const logIn = (req, res) => {
+  res.send("log in");
+};
+
+export const logOut = (req, res) => {
+  res.send("log out");
+};
