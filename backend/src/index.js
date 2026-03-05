3e1d9d639c7c170b7fef3f89f566449cf577eda7
hassan00-0 <hassanyassine0987@gmail.com>
2026-03-04 23:14:58 -0300

    added the updateProfile pic and checkAuth routes with the corresponding middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,15 @@ import express from "express";
 import authRouter from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import connectDb from "./lib/db.js";
-
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth", authRouter);
 
 app.listen(port, () => {
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -5,10 +5,14 @@ import {
   logOut,
   updateProfile,
 } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+
 const router = express.Router();
 
 router.post("/signup", signUp);
 router.post("/login", logIn);
 router.post("/logout", logOut);
 router.put("/update-profile", protectRoute, updateProfile);
+router.get("/check", protectRoute, checkAuth);
+
 export default router;
