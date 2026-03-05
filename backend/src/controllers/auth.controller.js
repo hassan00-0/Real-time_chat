0226ec77938dfacd6fc7ea2638a1d04c4119bf04
hassan00-0 <hassanyassine0987@gmail.com>
2026-03-04 23:14:58 -0300

    added the updateProfile pic and checkAuth routes with the corresponding middleware

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -75,3 +75,33 @@ export const logOut = (req, res) => {
     res.status(500).json({ message: "internal server error" });
   }
 };
+
+export const updateProfile = async (req, res) => {
+  const { profilePic } = req.body;
+  const userId = req.user._id;
+
+  try {
+    if (!profilePic) {
+      return res.status(400).json({ message: "Profile pic is required" });
+    }
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        profilePic: uploadResponse.secure_url,
+      },
+      { new: true },
+    );
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const checkAuth = (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
