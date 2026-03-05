eb7fcf574f19a4a1546b4909e5e050227cd1a3f7
hassan00-0 <hassanyassine0987@gmail.com>
2026-03-05 00:16:18 -0300

    add the message routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import authRouter from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import connectDb from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import { getMessages } from "./controllers/message.controller.js";
 dotenv.config();
 
 const app = express();
@@ -12,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
-
+app.use("/api/messages", getMessages);
 app.listen(port, () => {
   console.log("listening on port 5000.");
   connectDb();
