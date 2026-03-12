a79657fde37033f4af3ebfdcfcbf79a14cc451df
hassan00-0 <hassanyassine0987@gmail.com>
2026-03-12 14:17:43 -0300

    added the image preview logic

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import { X } from "lucide-react";
+import React, { useRef, useState } from "react";
 
 const MessageInput = () => {
-  return <div>MessageInput</div>;
+  const [text, setText] = useState("");
+  const [imagePreview, setImagePreview] = useState(null);
+  const filInput = useRef(null);
+
+  const handleImageChange = (e) => {};
+  const removeImage = () => {};
+  const handleSendMessage = (e) => {};
+
+  return (
+    <div className="p-4 w-full bg-base-300">
+      {/* image preview */}
+      {imagePreview && (
+        <div className="flex">
+          {/* image container */}
+          <div className="relative">
+            <img
+              src={imagePreview}
+              alt="Preview"
+              className="size-20 object-cover rounded-lg border border-zinc-700"
+            />
+          </div>
+
+          {/* close btn container */}
+          <button
+            onClick={removeImage}
+            className="absolute -top-1.5 -right-1.5 size-5 rounded-full bg-base-300 flex items-center justify-center"
+          >
+            <X className="size-3" />
+          </button>
+        </div>
+      )}
+
+      {/* handle sumbit form */}
+    </div>
+  );
 };
 
 export default MessageInput;
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set, get) => ({
   messages: [],
   users: [],
 
@@ -35,4 +35,14 @@ export const useChatStore = create((set) => ({
   },
 
   setSelectedUser: (selectedUser) => set({ selectedUser }),
+
+  sendMessage: async () => {
+    const { selectedUser, messages } = get();
+    try {
+      const res = await axiosInstance.get(`/messages/send/${selectedUser._id}`);
+      set({ messages: [...messages, res.data] });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  },
 }));
